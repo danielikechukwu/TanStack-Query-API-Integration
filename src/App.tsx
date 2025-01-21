84cd30b5efdeed521f71190fbecb0b799f3ceeac
Daniel ikechukwu <daniel.ikechukwu@avosoft.co.uk>
2025-01-21 07:01:31 +0000

    Set up a lint configuration, and implemented axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,62 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+export interface IData {
+  userId: number,
+  id: number,
+  title: string,
+  body: string
+}
 
 function App() {
 
+  const [data, setData] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+
+        setData(response.data);
+
+        setLoading(false);
+
+      })
+      .catch((error: Error) => {
+
+        console.log("An error occurred : ", error);
+
+      });
+
+  }, []);
+
+  if (loading) {
+
+    return <p>Loading...</p>;
+    
+  }
+
   return (
-    <>
-      <h4>Hello, world</h4>
-    </>
-  )
+
+    <div>
+
+      <h2>Returned data</h2>
+
+      <ul>
+
+        {data.map((item: IData) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+
+      </ul>
+
+    </div>
+
+  );
 }
 
-export default App
+export default App;
